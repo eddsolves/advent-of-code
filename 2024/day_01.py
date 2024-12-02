bbc761da5fe21de9eb94fa452ee353f2aa7216a8
eddsolves <hej@eddsolv.es>
2024-12-02 15:57:05 +0100

    Complete day 2

--- 2024/day_01.py
+++ 2024/day_01.py
@@ -2,46 +2,47 @@ def part_1():
     count = 0
     left_side = []
     right_side = []
-    
-    with open('day_01.txt', 'r') as f:
+
+    with open("./2024/day_01.txt", "r") as f:
         for line in f:
             ls, rs = map(int, line.split())
             left_side.append(ls)
             right_side.append(rs)
-    
+
     left_side.sort()
     right_side.sort()
-        
+
     for i, val in enumerate(left_side):
         count += abs(val - right_side[i])
-    
+
     return count
+
 
 def part_2():
     count = 0
     left_side = []
     right_side = []
-    
-    with open('day_01.txt', 'r') as f:
+
+    with open("./2024/day_01.txt", "r") as f:
         for line in f:
             ls, rs = map(int, line.split())
             left_side.append(ls)
             right_side.append(rs)
-    
+
     left_side.sort()
     right_side.sort()
-    
+
     similarity_score = {}
-    
+
     for value in right_side:
         if value in similarity_score:
             similarity_score[value] += 1
         else:
             similarity_score[value] = 1
-    
+
     for value in left_side:
         if value in similarity_score:
-            count += (value * similarity_score[value])
+            count += value * similarity_score[value]
 
     return count
 
---
+++ 2024/day_02.py
@@ -0,0 +1,68 @@
+def is_safe_ascending(level) -> bool:
+    lower_limit = 1
+    upper_limit = 3
+
+    for i, report in enumerate(level):
+        if i == 0:
+            continue
+        if lower_limit <= report - level[i - 1] <= upper_limit:
+            continue
+        else:
+            return False
+
+    return True
+
+def is_safe_descending(level) -> bool:
+    lower_limit = 1
+    upper_limit = 3
+
+    for i, report in enumerate(level):
+        if i == 0:
+            continue
+        if lower_limit <= level[i - 1] - report <= upper_limit:
+            continue
+        else:
+            return False
+
+    return True
+
+def part_1():
+    count = 0
+    levels = []
+
+    with open("./2024/day_02.txt", "r") as f:
+        for line in f:
+            levels.append(list(map(int, line.strip().split(" "))))
+
+    for level in levels:
+        if is_safe_ascending(level) or is_safe_descending(level):
+            count += 1
+
+    return count
+
+
+# Nothing about this is nice
+def part_2():
+    count = 0
+    levels = []
+
+    with open("./2024/day_02.txt", "r") as f:
+        for line in f:
+            levels.append(list(map(int, line.strip().split(" "))))
+
+    for level in levels:
+        if is_safe_ascending(level) or is_safe_descending(level):
+            count += 1
+            continue
+        
+        for i in range(len(level)):
+            temp_level = level[:i] + level[i+1:]
+            if is_safe_ascending(temp_level) or is_safe_descending(temp_level):
+                count += 1
+                break
+
+    return count
+
+
+print(part_1())
+print(part_2())

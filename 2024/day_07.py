fc84b7c37d5a788f628f70221c83fff271962c26
eddsolves <hej@eddsolv.es>
2024-12-07 22:26:01 +0100

    Finished day 7

--- 2024/day_07.py
+++ 2024/day_07.py
@@ -1,37 +1,37 @@
 from itertools import product
 
 
-def part_1():
+def solve():
     total = 0
-    inputs = {}
+    targets = []
+    allvalues = []
 
     with open("2024/data/day_07.txt", "r") as file:
         for line in file:
             target, values = line.strip().split(":")
-            inputs[int(target)] = list(map(int, values.strip().split(" ")))
+            targets.append(int(target))
+            allvalues.append(list(map(int, values.strip().split(" "))))
 
-    for target, values in inputs.items():
-        totals = {}
-
-        all_operations = list(
-            map(list, product(["sum", "mult"], repeat=len(values) - 1))
-        )
-
+    for target, values in zip(targets, allvalues):
+        all_operations = product(['sum', 'mult', 'concat'], repeat=len(values) - 1)
         for operations in all_operations:
             running_total = values[0]
+            found_concat = False
             for i, operation in enumerate(operations):
                 match operation:
                     case "sum":
                         running_total += values[i + 1]
                     case "mult":
                         running_total *= values[i + 1]
+                    case "concat":
+                        running_total = int(str(running_total) + str(values[i+1]))
 
-            totals[running_total] = (operations, values)
-
-        if target in totals:
-            total += target
+                        
+            if running_total == target and not found_concat:
+                total += target
+                break
 
     return total
 
 
-print(part_1())
+print(solve())
